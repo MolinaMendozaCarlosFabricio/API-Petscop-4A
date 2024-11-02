82a3288fb701240c42d1a91f459ea926cd0282c0
MolinaMendozaCarlosFabricio <233376@ids.upchiapas.edu.mx>
2024-11-01 18:12:34 -0600

    Trabajando en el módulo de usuarios, métodos de registro, login y obtener propio usuario listos

--- src/controllers/userController.py
+++ src/controllers/userController.py
@@ -3,26 +3,31 @@ from src.models.user import User, db
 from flask_jwt_extended import create_access_token, jwt_required, get_jwt_identity
 
 def crear_usuario(data):
-    nombre = data.get('nombre')
+    name = data.get('name')
+    lastname = data.get('lastname')
+    birthday = data.get('birthday')
     email = data.get('email')
     password = data.get('password')
+    type_user = data.get('type_user')
 
-    if not nombre or not email or not password:
+    if not name or not email or not password or not lastname or not birthday or not type_user:
         return jsonify({"mensaje": "Faltan campos obligatorios"}), 400
 
-    if User.query.filter_by(email=email).first():
+    if User.query.filter_by(email_user=email).first():
         return jsonify({"mensaje": "El email ya está registrado"}), 400
 
-    nuevo_usuario = User(nombre=nombre, email=email, password=password)
+    nuevo_usuario = User(name=name, lastname=lastname, birthday=birthday, email=email, password=password, type_user=type_user)
     db.session.add(nuevo_usuario)
     db.session.commit()
     return jsonify({
         "mensaje": "Usuario creado con bcrypt",
-        "id": nuevo_usuario.id,
-        "nombre": nuevo_usuario.nombre,
-        "email": nuevo_usuario.email
+        "id": nuevo_usuario.id_user,
+        "nombre": nuevo_usuario.name_user,
+        "apellido": nuevo_usuario.lastname_user,
+        "email": nuevo_usuario.email_user
     }), 201
 
+"""
 def crear_usuario_base(data):
     nombre = data.get('nombre')
     email = data.get('email')
@@ -40,16 +45,17 @@ def crear_usuario_base(data):
         "nombre": nuevo_usuario.nombre,
         "email": nuevo_usuario.email
     }), 201
+"""
 
 def login_usuario(data):
     email = data.get('email')
     password = data.get('password')
-    user = User.query.filter_by(email=email).first()
+    user = User.query.filter_by(email_user=email).first()
     if not user:
         return jsonify({"mensaje": "Credenciales inválidas"}), 401
     if not user.check_password(password):
         return jsonify({"mensaje": "Credenciales inválidas"}), 401
-    access_token = create_access_token(identity=user.id)
+    access_token = create_access_token(identity=user.id_user)
     return jsonify({"mensaje": "Inicio de sesión exitoso", "token": access_token}), 200
 
 @jwt_required()
@@ -59,9 +65,9 @@ def obtener_usuario():
     if not user:
         return jsonify({"mensaje": "Usuario no encontrado"}), 404
     return jsonify({
-        "id": user.id,
-        "nombre": user.nombre,
-        "email": user.email
+        "id": user.id_user,
+        "nombre": user.name_user,
+        "email": user.email_user
     }), 200
 
 @jwt_required()
