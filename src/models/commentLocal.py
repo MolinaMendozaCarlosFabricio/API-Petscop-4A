3def9d8ffb622e51bff58ca0e6128049782c0f9e
MolinaMendozaCarlosFabricio <233376@ids.upchiapas.edu.mx>
2024-11-09 14:20:50 -0600

    Cambiando tipo de dato del id de post y locales/servicios

--- src/models/commentLocal.py
+++ src/models/commentLocal.py
@@ -11,7 +11,7 @@ class Response_local(db.Model):
     __tablename__ = 'response_to_local'
     __table_args__ = {'schema': schema_name }
     id_response_local = db.Column(db.Integer, primary_key=True)
-    id_local_to_comment = db.Column(db.Integer, nullable=False)
+    id_local_to_comment = db.Column(db.String, nullable=False)
     id_response_to_local = db.Column(db.Integer, ForeignKey(f'{schema_name}.comments.id_comment'))
 
     comment = relationship('Comment', back_populates='response_to_local', lazy='joined')
